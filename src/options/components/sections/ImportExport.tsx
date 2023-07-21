e0f9284df85598b6d730fa0b97cc72dd06f36723
MadhuSaini22 <madhusaini46810@gmail.com>
2023-07-21 12:47:40 +0530

    add: import export ui

--- src/options/components/sections/ImportExport.tsx
+++ src/options/components/sections/ImportExport.tsx
@@ -1,12 +1,28 @@
-import { useRecoilState } from 'recoil'
 import { useContent } from '../../../customHooks/use-content'
 import useOpJobs from '../../../customHooks/use-option-jobs'
-import { proposals } from '../../atoms'
+import { DocumentTextIcon } from '@heroicons/react/24/outline'
+import { useState } from 'react'
+
+interface Proposal {
+  name: string
+}
 
 export const ImportExportSection: React.FC<{}> = ({}) => {
-  const [allProposals, setAllProposals] = useRecoilState(proposals)
   const { setLocalJobs, setLocalKeywords } = useOpJobs()
   const { setProposal, getProposals } = useContent()
+
+  const [selectedImg, setSelectedImg] = useState<{
+    proposal: Proposal | null
+    keyword: Proposal | null
+  }>({
+    proposal: null,
+    keyword: null,
+  })
+
+  const ImportExportConfig = [
+    { id: 'keywords', label: 'Keywords', inputId: 'keywordInput', key: 'keyword' },
+    { id: 'proposals', label: 'Profiles', inputId: 'proposalInput', key: 'proposal' },
+  ]
 
   function exportToJson(type: string) {
     chrome.storage.local.get(['keywords', 'proposals']).then((res) => {
@@ -35,14 +51,12 @@ export const ImportExportSection: React.FC<{}> = ({}) => {
         ? document.getElementById('keywordInput')
         : document.getElementById('proposalInput')
     ) as HTMLInputElement
-    console.log(type, InputElem.files)
     if (InputElem?.files?.length && InputElem?.files?.length > 0) {
       const file = InputElem.files[0]
       const reader = new FileReader()
       reader.onload = async function (event: any) {
         const jsonData = JSON.parse(event.target.result)
         // Process the JSON data
-        console.log({ jsonData })
         if (jsonData.length > 0) {
           for (const i of jsonData[1]) {
             if (type == 'keywords') {
@@ -60,53 +74,49 @@ export const ImportExportSection: React.FC<{}> = ({}) => {
   }
   return (
     <div className="flex flex-col justify-between items-center p-4 mx-auto container">
-      <div className="flex flex-col text-xl py-4">
-        <h2 className="text-2xl font-bold py-4">Keywords</h2>
-        <div className="flex">
-          <button className="flex gap-x-3">
-            <span className="font-bold">Import</span>
-            <input
-              type="file"
-              id="keywordInput"
-              onChange={() => importFromJson('keywords')}
-              className="block w-full text-sm text-slate-500
-        file:mr-4 file:py-2 file:px-4 file:rounded-md
-        file:border-0 file:text-sm file:font-semibold
-        file:bg-green-50 file:text-green-700
-        hover:file:bg-green-100 cursor-pointer"
-            />
-          </button>
-          <button
-            onClick={() => exportToJson('keywords')}
-            className="font-bold rounded-md border border-green-400 px-6 py-2"
-          >
-            <a id="export"></a>Export Keywords
-          </button>
-        </div>
-      </div>
-      <div className="flex flex-col text-xl py-4">
-        <h2 className="text-2xl font-bold py-4">Profiles</h2>
-        <div className="flex">
-          <button className="flex gap-x-3">
-            <span className="font-bold">Import</span>
-            <input
-              type="file"
-              onChange={() => importFromJson('proposals')}
-              id="proposalInput"
-              className="block w-full text-sm text-slate-500
-        file:mr-4 file:py-2 file:px-4 file:rounded-md
-        file:border-0 file:text-sm file:font-semibold
-        file:bg-green-50 file:text-green-700
-        hover:file:bg-green-100 cursor-pointer"
-            />
-          </button>
-          <button
-            onClick={() => exportToJson('proposals')}
-            className="font-bold rounded-md border border-green-400 px-6 py-2"
-          >
-            <a id="export"></a>Export Profiles
-          </button>
-        </div>
+      <div className="w-[600px] space-y-5">
+        {ImportExportConfig.map((elem) => (
+          <div className="col-span-full mt-8 space-y-5" key={elem.id}>
+            <h2 className="text-2xl text-center font-bold ">{elem.label}</h2>
+            <div className="mt-2 flex justify-center rounded-lg border border-dashed border-gray-400 p-6">
+              <div className="text-center">
+                <DocumentTextIcon className="mx-auto h-12 w-12 text-gray-300" aria-hidden="true" />
+                <div className="mt-4 flex text-sm leading-6 text-gray-500">
+                  <label
+                    htmlFor={elem.inputId}
+                    className="relative cursor-pointer rounded-md font-semibold text-green-600 focus-within:outline-none focus-within:ring-1 focus-within:ring-green-600 focus-within:ring-offset-1"
+                  >
+                    <div className="text-green-500 w-[170px] hover:text-green-600">
+                      {selectedImg && selectedImg[elem.key as keyof typeof selectedImg]?.name
+                        ? selectedImg[elem.key as keyof typeof selectedImg]?.name
+                        : 'Upload a file'}
+                    </div>
+
+                    <input
+                      id={elem.inputId}
+                      onChange={(e: any) => {
+                        importFromJson(elem.id)
+                        setSelectedImg((prev) => ({ ...prev, [elem.key]: e.target.files[0] }))
+                      }}
+                      name={elem.inputId}
+                      type="file"
+                      className="sr-only"
+                    />
+                  </label>
+                </div>
+                <p className="text-sm leading-5 text-gray-500">Please upload json files only.</p>
+              </div>
+            </div>
+            <div className="flex justify-end">
+              <button
+                onClick={() => exportToJson(elem.id)}
+                className="font-bold w-[200px] rounded-md border text-lg border-green-400 px-6 py-2"
+              >
+                <a id="export"></a>Export {elem.label}
+              </button>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   )
