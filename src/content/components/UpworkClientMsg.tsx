99d6d0d82a9863c0fe0d402af9f253a53b4f5d5c
Krish <kpkapadia@enacton.com>
2023-08-01 16:11:07 +0530

    feat: text area implemented

--- src/content/components/UpworkClientMsg.tsx
+++ src/content/components/UpworkClientMsg.tsx
@@ -72,11 +72,11 @@ const UpworkClientMsg = () => {
             </label>
           </div>
           <div className="md:w-2/3">
-            <input
+            <textarea
               className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
               id="message"
               name="message"
-              type="text"
+              rows={3}
             />
           </div>
         </div>
